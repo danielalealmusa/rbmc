697ad2638672d04268f2281f57fcc3757b500a00
danielalealmusa <musa@unifesp.br>
2025-08-12 10:41:19 -0300

    ok# Commit message for your changes

--- backend-api/server.js
+++ backend-api/server.js
@@ -14,7 +14,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000; // Porta do servidor
-app.use(express.static('front-end'));
+
 app.use(express.urlencoded({ extended: true })); 
 
 app.use(cors({
